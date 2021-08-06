40950084e9ee7f0f965f77952d79f06e00a75e21
MinerSebas <scherthan_sebastian@web.de>
2021-08-06 20:04:34 +0200

    Refactor to allow running other Lints

--- src/unnecessary_with.rs
+++ src/unnecessary_with.rs
@@ -50,7 +50,12 @@ impl<'hir> LateLintPass<'hir> for UnnecessaryWith {
         _: HirId,
     ) {
         for typ in decl.inputs {
-            recursively_search_type(ctx, typ, bevy_paths::QUERY, check_for_unnecesarry_with);
+            recursively_search_type(
+                ctx,
+                typ,
+                bevy_paths::QUERY,
+                check_for_unnecesarry_query_parameters,
+            );
         }
     }
 
@@ -58,18 +63,15 @@ impl<'hir> LateLintPass<'hir> for UnnecessaryWith {
         // Todo: Filter out Types that dont implement SystemParam
         // -> Is it possible to go from rustc_hir::Ty to rustc_middle::Ty?
         // Required for using clippy_utils::ty::implements_trait.
-        match data {
-            VariantData::Struct(fields, _) => {
-                for field in *fields {
-                    recursively_search_type(
-                        ctx,
-                        field.ty,
-                        bevy_paths::QUERY,
-                        check_for_unnecesarry_with,
-                    )
-                }
+        if let VariantData::Struct(fields, _) = data {
+            for field in *fields {
+                recursively_search_type(
+                    ctx,
+                    field.ty,
+                    bevy_paths::QUERY,
+                    check_for_unnecesarry_query_parameters,
+                )
             }
-            _ => (),
         }
     }
 }
@@ -98,15 +100,16 @@ fn recursively_search_type<'hir, T: Fn(&LateContext<'hir>, &'hir Ty<'hir>) + Cop
     }
 }
 
-fn check_for_unnecesarry_with<'hir>(ctx: &LateContext<'hir>, query: &'hir Ty<'hir>) {
+fn check_for_unnecesarry_query_parameters<'hir>(ctx: &LateContext<'hir>, query: &'hir Ty<'hir>) {
     if let Some((world, Some(filter))) = bevy_helpers::get_generics_of_query(ctx, query) {
-        let mut required_types = Vec::new();
-        let mut with_types = Vec::new();
+        let mut parameters = QueryParameters::default();
 
         match &world.kind {
             TyKind::Rptr(_, mut_type) => {
                 if let Some(def_id) = bevy_helpers::get_def_id_of_referenced_type(&mut_type) {
-                    required_types.push(def_id);
+                    parameters
+                        .data_querys
+                        .push(QueryParameter::new(def_id, world.span));
                 }
             }
             TyKind::Tup(types) => {
@@ -114,7 +117,9 @@ fn check_for_unnecesarry_with<'hir>(ctx: &LateContext<'hir>, query: &'hir Ty<'hi
                     if let TyKind::Rptr(_, mut_type) = &typ.kind {
                         if let Some(def_id) = bevy_helpers::get_def_id_of_referenced_type(&mut_type)
                         {
-                            required_types.push(def_id);
+                            parameters
+                                .data_querys
+                                .push(QueryParameter::new(def_id, typ.span));
                         }
                     }
                 }
@@ -125,11 +130,13 @@ fn check_for_unnecesarry_with<'hir>(ctx: &LateContext<'hir>, query: &'hir Ty<'hi
         match &filter.kind {
             TyKind::Path(QPath::Resolved(_, path)) => {
                 if bevy_helpers::path_matches_symbol_path(ctx, path, bevy_paths::OR) {
-                    with_types.extend(check_or_filter(ctx, path));
+                    parameters.with_querys.extend(check_or_filter(ctx, path));
                 }
                 if bevy_helpers::path_matches_symbol_path(ctx, path, bevy_paths::WITH) {
                     if let Some(def_id) = bevy_helpers::get_def_id_of_first_generic_arg(path) {
-                        with_types.push((def_id, filter.span));
+                        parameters
+                            .with_querys
+                            .push(QueryParameter::new(def_id, filter.span));
                     }
                 }
             }
@@ -137,7 +144,7 @@ fn check_for_unnecesarry_with<'hir>(ctx: &LateContext<'hir>, query: &'hir Ty<'hi
                 for typ in *types {
                     if let TyKind::Path(QPath::Resolved(_, path)) = typ.kind {
                         if bevy_helpers::path_matches_symbol_path(ctx, path, bevy_paths::OR) {
-                            with_types.extend(check_or_filter(ctx, path));
+                            parameters.with_querys.extend(check_or_filter(ctx, path));
                         }
                         if bevy_helpers::path_matches_symbol_path(ctx, path, bevy_paths::ADDED)
                             || bevy_helpers::path_matches_symbol_path(
@@ -149,14 +156,18 @@ fn check_for_unnecesarry_with<'hir>(ctx: &LateContext<'hir>, query: &'hir Ty<'hi
                             if let Some(def_id) =
                                 bevy_helpers::get_def_id_of_first_generic_arg(path)
                             {
-                                required_types.push(def_id);
+                                parameters
+                                    .change_detection_querys
+                                    .push(QueryParameter::new(def_id, typ.span));
                             }
                         }
                         if bevy_helpers::path_matches_symbol_path(ctx, path, bevy_paths::WITH) {
                             if let Some(def_id) =
                                 bevy_helpers::get_def_id_of_first_generic_arg(path)
                             {
-                                with_types.push((def_id, typ.span));
+                                parameters
+                                    .with_querys
+                                    .push(QueryParameter::new(def_id, typ.span));
                             }
                         }
                     }
@@ -165,22 +176,12 @@ fn check_for_unnecesarry_with<'hir>(ctx: &LateContext<'hir>, query: &'hir Ty<'hi
             _ => (),
         }
 
-        for with_type in with_types {
-            if required_types.contains(&with_type.0) {
-                span_lint(
-                    ctx,
-                    UNNECESSARY_WITH,
-                    with_type.1,
-                    "Unnecessary `With` Filter",
-                );
-            }
-        }
+        parameters.check_for_unnecesarry_with(ctx);
     }
 }
 
-fn check_or_filter<'hir>(ctx: &LateContext<'hir>, path: &Path) -> Vec<(DefId, Span)> {
-    let mut local_required_types = Vec::new();
-    let mut local_with_types = Vec::new();
+fn check_or_filter<'hir>(ctx: &LateContext<'hir>, path: &Path) -> Vec<QueryParameter> {
+    let mut parameters = QueryParameters::default();
 
     if let Some(segment) = path.segments.iter().last() {
         if let Some(generic_args) = segment.args {
@@ -198,33 +199,80 @@ fn check_or_filter<'hir>(ctx: &LateContext<'hir>, path: &Path) -> Vec<(DefId, Sp
                                 if let Some(def_id) =
                                     bevy_helpers::get_def_id_of_first_generic_arg(path)
                                 {
-                                    local_required_types.push(def_id);
+                                    parameters
+                                        .change_detection_querys
+                                        .push(QueryParameter::new(def_id, typ.span));
                                 }
                             }
                             if bevy_helpers::path_matches_symbol_path(ctx, path, bevy_paths::WITH) {
                                 if let Some(def_id) =
                                     bevy_helpers::get_def_id_of_first_generic_arg(path)
                                 {
-                                    local_with_types.push((def_id, typ.span));
+                                    parameters
+                                        .with_querys
+                                        .push(QueryParameter::new(def_id, typ.span));
                                 }
                             }
                         }
                     }
 
-                    for with_type in &local_with_types {
-                        if local_required_types.contains(&with_type.0) {
-                            span_lint(
-                                ctx,
-                                UNNECESSARY_WITH,
-                                with_type.1,
-                                "Unnecessary `With` Filter",
-                            );
-                        }
-                    }
+                    parameters.check_for_unnecesarry_with(ctx);
                 }
             }
         }
     }
 
-    local_with_types
+    parameters.with_querys
+}
+
+#[derive(Debug)]
+struct QueryParameter {
+    def_id: DefId,
+    span: Span,
+    lint_triggered: bool,
+}
+
+impl QueryParameter {
+    fn new(def_id: DefId, span: Span) -> Self {
+        Self {
+            def_id,
+            span,
+            lint_triggered: false,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct QueryParameters {
+    data_querys: Vec<QueryParameter>,
+    optional_querys: Vec<QueryParameter>,
+    with_querys: Vec<QueryParameter>,
+    without_querys: Vec<QueryParameter>,
+    with_bundle_querys: Vec<QueryParameter>,
+    change_detection_querys: Vec<QueryParameter>,
+}
+
+impl QueryParameters {
+    fn check_for_unnecesarry_with<'hir>(&mut self, ctx: &LateContext<'hir>) {
+        let iterator = self.data_querys.iter().chain(&self.change_detection_querys);
+
+        for mut with_query in self
+            .with_querys
+            .iter_mut()
+            .filter(|parameter| !parameter.lint_triggered)
+        {
+            if iterator
+                .clone()
+                .any(|parameter| parameter.def_id == with_query.def_id)
+            {
+                span_lint(
+                    ctx,
+                    UNNECESSARY_WITH,
+                    with_query.span,
+                    "Unnecessary `With` Filter",
+                );
+                with_query.lint_triggered = true;
+            }
+        }
+    }
 }
