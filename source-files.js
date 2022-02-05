23a0188baeefcd4e34a1effc5c721da668ba5b46
MinerSebas <MinerSebas@users.noreply.github.com>
2022-02-05 18:23:03 +0000

    deploy: b49563d3fd5a4fc0ca229c3f1276edf73b057abb

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bevy_lint"] = {"name":"","dirs":[{"name":"system_lints","files":["mod.rs","model.rs","query_lints.rs"]}],"files":["app_lints.rs","bevy_paths.rs","bundle_lints.rs","label_lints.rs","lib.rs","mixed_ty.rs"]};
+createSourceSidebar();
