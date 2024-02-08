53ae981e2176183733dca685fdd5cdad0ab1bcd1
MinerSebas <MinerSebas@users.noreply.github.com>
2024-02-08 16:05:37 +0000

    deploy: 33ad89d888ffbca384c2601b863048b8d0bc5143

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["bevy_lint",["",[["system_lints",[],["mod.rs","model.rs","query_lints.rs"]]],["app_lints.rs","bevy_paths.rs","bundle_lints.rs","lib.rs","mixed_ty.rs"]]]\
+]'));
+createSrcSidebar();
