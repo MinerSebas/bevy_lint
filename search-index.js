6f6805b3eca6b65544a8579c228e5bc8e44ef10d
MinerSebas <MinerSebas@users.noreply.github.com>
2024-05-08 14:18:06 +0000

    deploy: 8f550a5b903972025c315b0bee468edceeff2eda

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["bevy_lint",{"t":"JJJJJJJJ","n":["BUNDLE_WITH_INCOMPLETE_TRANSFORMS","EMPTY_QUERY","INSERT_RESOURCE_WITH_DEFAULT","UNNECESSARY_ADDED","UNNECESSARY_CHANGED","UNNECESSARY_OPTION","UNNECESSARY_OR","UNNECESSARY_WITH"],"q":[[0,"bevy_lint"],[8,"bevy_lint::bundle_lints"],[9,"bevy_lint::system_lints::query_lints"],[10,"bevy_lint::app_lints"]],"i":[0,0,0,0,0,0,0,0],"f":"````````","D":"A`","p":[],"r":[[0,8],[1,9],[2,10],[3,9],[4,9],[5,9],[6,9],[7,9]],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAAAEAAAAAAA"}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
